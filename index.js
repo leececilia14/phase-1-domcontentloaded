18d950f5c21e9463614f6a01a81f802a52ec21b6
Cecilia Lee <leececilia14@gmail.com>
2021-04-12 08:19:38 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function() { 
+    updatedDOM()
+} );
+
+function updatedDOM() {
+    document.getElementById("text")
+    .innerHTML="This is really cool!"; 
+
+    
+}
